1dee7c2f4b3d7a94b8c17836603d4f3d77f1824a
Niswahtrs <niswahayun8@gmail.com>
2024-08-19 08:38:10 +0700

    style: styling web

--- assets/js/script-2.js
+++ assets/js/script-2.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('.navbar');
 window.addEventListener('scroll', function(){
-    if(this.scrollY > 200){
+    if(this.scrollY > 10){
         nav.classList.add('nav-color');
     } else {
         nav.classList.remove('nav-color');
